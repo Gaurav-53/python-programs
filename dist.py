351525165ba76d3b8ac41d041e30c0a97e0a4be8
Gaurav-53 <60250518+Gaurav-53@users.noreply.github.com>
2020-01-24 11:51:52 +0530

    distance between two points

---
+++ dist.py
@@ -0,0 +1,6 @@
+x1=int(input("enter the value:"))
+x2=int(input("enter the value:"))
+y1=int(input("enter the value:"))
+y2=int(input("enter the value:"))
+distance=(x2-x1)**2+(y1-y2)**2
+print ("distance between two pts is %d"%distance)
